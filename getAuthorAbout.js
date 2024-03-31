61ad3334a1e8eb2500bf332b1b1ecbe7c0d3f53e
Khaleeq <kkhaliqq2711@gmail.com>
2024-03-31 15:51:15 +0500

    2nd Commit without node_modules

--- getAuthorAbout.js
+++ getAuthorAbout.js
@@ -16,6 +16,28 @@ const scrapping = async () => {
   const getAuthor = async () => {
     const allData = [];
 
+    await page.waitForSelector(".quote");
+    const quoteElements = await page.$(".quote");
+
+    const authorElement = await quoteElements.$(".author");
+    let author, date;
+    if (authorElement)
+      author = await page.evaluate((a) => a.innerText, authorElement);
+
+    await page.waitForSelector(".quote > span a");
+    const btn = await page.$(".quote > span > a");
+    await btn.click();
+
+    await page.waitForSelector(".author-born-date");
+    await new Promise((resolve) => setTimeout(resolve, 2500));
+
+    const dateEl = await page.$(".author-born-date");
+    if (dateEl) {
+      date = await page.evaluate((d) => d.innerText, dateEl);
+    }
+    await page.goBack();
+    allData.push({ author, date });
+
     return allData;
   };
   console.log(await getAuthor());
